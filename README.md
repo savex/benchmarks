8f9d56642874fd8a70528c5490c36e057a2f1455
Alex <osavatieiev@mirantis.com>
2021-04-13 16:53:04 -0500

    Updates source linux, fio source, test vars and output

--- README.md
+++ README.md
@@ -1,14 +1,27 @@
-# dbench
+# fio container
 Benchmark Kubernetes persistent disk volumes with `fio`: Read/write IOPS, bandwidth MB/s and latency.
+
+Based on the excellent work of
+* Lee Liu (LogDNA)
+* [Alexis Turpin](https://github.com/alexis-turpin)
+
+# Updates and changes
+Here is the list of updates made so far
+ * Removed Alpine linux dependency, based on Ubuntu:20.04
+ * Clone-n-make of fio every time when docker container is built
+ * Cleaner output of tests
+ * fio output stored in mounted folder
+ * Current versions are 'docker-fio-2-17' and 'docker-fio-3.26'
 
 # Usage
 
-1. Download [dbench.yaml](https://raw.githubusercontent.com/logdna/dbench/master/dbench.yaml) and edit the `storageClassName` to match your Kubernetes provider's Storage Class `kubectl get storageclasses`
-2. Deploy Dbench using: `kubectl apply -f dbench.yaml`
-3. Once deployed, the Dbench Job will:
+1. Download [fio.yaml](https://raw.githubusercontent.com/savex13/benchmarks/master/fio.yaml) and edit the `storageClassName` to match your Kubernetes provider's Storage Class `kubectl get storageclasses`
+2. Deploy fio using: `kubectl apply -f fio.yaml`
+3. Once deployed, the fio Job will:
     * provision a Persistent Volume of `1000Gi` (default) using `storageClassName: ssd` (default)
     * run a series of `fio` tests on the newly provisioned disk
     * currently there are 9 tests, 15s per test - total runtime is ~2.5 minutes
+    * each test can be configured with specific block size and depth instead of defaults
 4. Follow benchmarking progress using: `kubectl logs -f job/dbench` (empty output means the Job not yet created, or `storageClassName` is invalid, see Troubleshooting below)
 5. At the end of all tests, you'll see a summary that looks similar to this:
 ```
@@ -20,6 +33,7 @@ Average Latency (usec) Read/Write: 183.07/76.91
 Sequential Read/Write: 536MiB/s / 512MiB/s
 Mixed Random Read/Write IOPS: 43.1k/14.4k
 ```
+If latency is not shown for you, then it is probably something wrong with measuring it via kernel/device
 6. Once the tests are finished, clean up using: `kubectl delete -f dbench.yaml` and that should deprovision the persistent disk and delete it to minimize storage billing.
 
 ## Notes / Troubleshooting
@@ -27,12 +41,7 @@ Mixed Random Read/Write IOPS: 43.1k/14.4k
 * If the Persistent Volume Claim is stuck on Pending, it's likely you didn't specify a valid Storage Class. Double check using `kubectl get storageclasses`. Also check that the volume size of `1000Gi` (default) is available for provisioning.
 * It can take some time for a Persistent Volume to be Bound and the Kubernetes Dashboard UI will show the Dbench Job as red until the volume is finished provisioning.
 * It's useful to test multiple disk sizes as most cloud providers price IOPS per GB provisioned. So a `4000Gi` volume will perform better than a `1000Gi` volume. Just edit the yaml, `kubectl delete -f dbench.yaml` and run `kubectl apply -f dbench.yaml` again after deprovision/delete completes.
-* A list of all `fio` tests are in [docker-entrypoint.sh](https://github.com/logdna/dbench/blob/master/docker-entrypoint.sh).
-
-## Contributors
-
-* Lee Liu (LogDNA)
-* [Alexis Turpin](https://github.com/alexis-turpin)
+* A list of all `fio` tests are in [fio-entrypoint.sh](https://github.com/savex13/benchmarks/blob/master/fio-entrypoint.sh).
 
 ## License
 
---
+++ fio-entrypoint.sh
@@ -0,0 +1,169 @@
+#!/usr/bin/env bash
+set -e
+
+function get_var() {
+    # if $1 and $2 exists, then we must use $1
+    # if there is only $1, then $1 is actually empty and it is a $2 shown as $1
+    # if nothing, then just break exit
+    if [ -z $2 ]; then
+        if [ -z $1 ]; then
+            echo "Error in script while setting vars"
+            exit 1
+        else
+            echo $1
+        fi
+    else
+       echo $1
+    fi
+}
+
+# Global vars
+FIO_MOUNTPOINT=$( get_var $FIO_MOUNTPOINT "/tmp")
+FIO_SIZE=$( get_var $FIO_SIZE "2G")
+FIO_OFFSET_INCREMENT=$( get_var $FIO_OFFSET_INCREMENT "500M")
+FIO_DIRECT=$( get_var $FIO_DIRECT "1")
+FIO_IODEPTH=$( get_var $FIO_IODEPTH "64")
+
+# IOPS test
+IOPS_BS=$( get_var $IOPS_BS "4k")
+IOPS_DP=$( get_var $IOPS_DP $FIO_IODEPTH)
+
+# Bandwidth test
+BW_BS=$( get_var $BW_BS "128k")
+BW_DP=$( get_var $BW_DP $FIO_IODEPTH)
+
+# Latency test
+LAT_BS=$( get_var $LAT_BS "4k")
+LAT_DP=$( get_var $LAT_DP "4")
+
+# Sequential test
+SEQ_BS=$( get_var $SEQ_BS "1M")
+SEQ_DP=$( get_var $SEQ_DP "16")
+SEQ_JOBS=$( get_var $SEQ_JOBS "4")
+
+# Mixed test
+MIX_BS=$( get_var $MIX_BS "4k")
+MIX_DP=$( get_var $MIX_DP $FIO_IODEPTH)
+MIX_RWMIXREAD=$( get_var $MIX_RWMIXREAD "75")
+
+function run_fio() {
+    fio --randrepeat=0 --verify=0 --ioengine=libaio --direct=$FIO_DIRECT --gtod_reduce=1 --name=$1 --filename=$FIO_MOUNTPOINT/fiotest --bs=$2 --iodepth=$3 --size=$FIO_SIZE --readwrite=$4 --time_based --ramp_time=2s --runtime=15s
+}
+
+function run_fio_seq() {
+    fio --randrepeat=0 --verify=0 --ioengine=libaio --direct=$FIO_DIRECT --gtod_reduce=1 --name=$1 --filename=$FIO_MOUNTPOINT/fiotest --bs=$2 --iodepth=$3 --size=$FIO_SIZE --readwrite=$4 --time_based --ramp_time=2s --runtime=15s --thread --numjobs=${SEQ_JOBS} --offset_increment=$FIO_OFFSET_INCREMENT
+}
+
+# Tests
+function test_iops_read() {
+    READ_IOPS=$(run_fio read_iops $IOPS_BS $IOPS_DP randread)
+    echo "$READ_IOPS" >$FIO_MOUNTPOINT/test_iops_read.log
+    echo "$READ_IOPS"|grep -E 'read ?:'|grep -Eoi 'IOPS=[0-9k.]+'|cut -d'=' -f2
+}
+
+function test_iops_write() {
+    WRITE_IOPS=$(run_fio write_iops $IOPS_BS $IOPS_DP randwrite)
+    echo "$WRITE_IOPS" >$FIO_MOUNTPOINT/test_iops_write.log
+    echo "$WRITE_IOPS"|grep -E 'write:'|grep -Eoi 'IOPS=[0-9k.]+'|cut -d'=' -f2
+}
+
+function test_bw_read() {
+    READ_BW=$(run_fio read_bw $BW_BS $BW_DP randread)
+    echo "$READ_BW" >$FIO_MOUNTPOINT/test_bw_read.log
+    echo "$READ_BW"|grep -E 'read ?:'|grep -Eoi 'BW=[0-9GMKiBs/.]+'|cut -d'=' -f2
+}
+
+function test_bw_write() {
+    WRITE_BW=$(run_fio write_bw $BW_BS $BW_DP randwrite)
+    echo "$WRITE_BW" >$FIO_MOUNTPOINT/test_bw_write.log
+    echo "$WRITE_BW"|grep -E 'write:'|grep -Eoi 'BW=[0-9GMKiBs/.]+'|cut -d'=' -f2
+}
+
+function test_latency_read() {
+    READ_LATENCY=$(run_fio read_latency $LAT_BS $LAT_DP randread)
+    echo "$READ_LATENCY" >$FIO_MOUNTPOINT/test_latency_read.log
+    echo "$READ_LATENCY"|grep ' lat.*avg'|grep -Eoi 'avg=[0-9.]+'|cut -d'=' -f2
+}
+
+function test_latency_write() {
+    WRITE_LATENCY=$(run_fio write_latency $LAT_BS $LAT_DP randwrite)
+    echo "$WRITE_LATENCY" >$FIO_MOUNTPOINT/test_latency_write.log
+    echo "$WRITE_LATENCY"|grep ' lat.*avg'|grep -Eoi 'avg=[0-9.]+'|cut -d'=' -f2
+}
+
+function test_seq_read() {
+    READ_SEQ=$(run_fio_seq read_seq $SEQ_BS $SEQ_DP read)
+    echo "$READ_SEQ" >$FIO_MOUNTPOINT/test_seq_read.log
+    echo "$READ_SEQ"|grep -E 'READ:'|grep -Eoi '(aggrb|bw)=[0-9GMKiBs/.]+'|cut -d'=' -f2
+}
+
+function test_seq_write() {
+    WRITE_SEQ=$(run_fio_seq write_seq $SEQ_BS $SEQ_DP write)
+    echo "$WRITE_SEQ" >$FIO_MOUNTPOINT/test_seq_write.log
+    echo "$WRITE_SEQ"|grep -E 'WRITE:'|grep -Eoi '(aggrb|bw)=[0-9GMKiBs/.]+'|cut -d'=' -f2
+}
+
+# Main
+echo "Working dir: ${FIO_MOUNTPOINT}"
+echo "Size: ${FIO_SIZE}"
+echo "Offset increment: ${FIO_OFFSET_INCREMENT}"
+echo "Direct flag set to ${FIO_DIRECT}"
+echo "Default iodepth: $FIO_IODEPTH"
+if [ "$FIO_QUICK" == "" ] || [ "$FIO_QUICK" == "no" ]; then
+    echo "Quick test: no"
+else
+    echo "Quick test: yes"
+fi
+echo
+
+if [ "$1" = 'fio' ]; then
+
+    echo "# Testing Read IOPS ($IOPS_BS/$IOPS_DP/$FIO_SIZE)"
+    READ_IOPS_VAL=$(test_iops_read)
+
+    echo "# Testing Write IOPS ($IOPS_BS/$IOPS_DP/$FIO_SIZE)"
+    WRITE_IOPS_VAL=$(test_iops_write)
+
+    echo "# Testing Read Bandwidth ($BW_BS/$BW_DP/$FIO_SIZE)"
+    READ_BW_VAL=$(test_bw_read)
+
+    echo "# Testing Write Bandwidth ($BW_BS/$BW_DP/$FIO_SIZE)"
+    WRITE_BW_VAL=$(test_bw_write)
+
+    if [ "$FIO_QUICK" == "" ] || [ "$FIO_QUICK" == "no" ]; then
+        echo "# Testing Read Latency ($LAT_BS/$LAT_DP/$FIO_SIZE)"
+        READ_LATENCY_VAL=$(test_latency_read)
+
+        echo "# Testing Write Latency ($LAT_BS/$LAT_DP/$FIO_SIZE)"
+        WRITE_LATENCY_VAL=$(test_latency_write)
+
+        echo "# Testing Read Sequential Speed ($SEQ_BS/$SEQ_DP/$FIO_SIZE, $SEQ_JOBS jobs, offset $FIO_OFFSET_INCREMENT)"
+        READ_SEQ_VAL=$(test_seq_read)
+
+        echo "# Testing Write Sequential Speed ($SEQ_BS/$SEQ_DP/$FIO_SIZE, $SEQ_JOBS jobs, offset $FIO_OFFSET_INCREMENT)"
+        WRITE_SEQ_VAL=$(test_seq_write)
+
+        echo "# Testing Read/Write Mixed ($MIX_BS/$MIX_DP/$FIO_SIZE, rwmixread at $MIX_RWMIXREAD%)"
+        RW_MIX=$(fio --randrepeat=0 --verify=0 --ioengine=libaio --direct=$FIO_DIRECT --gtod_reduce=1 --name=rw_mix --filename=$FIO_MOUNTPOINT/fiotest --bs=$MIX_BS --iodepth=$MIX_DP --size=$FIO_SIZE --readwrite=randrw --rwmixread=$MIX_RWMIXREAD --time_based --ramp_time=2s --runtime=15s)
+        echo "$RW_MIX" >$FIO_MOUNTPOINT/test_mix_readwrite.log
+        RW_MIX_R_IOPS=$(echo "$RW_MIX"|grep -E 'read ?:'|grep -Eoi 'IOPS=[0-9k.]+'|cut -d'=' -f2)
+        RW_MIX_W_IOPS=$(echo "$RW_MIX"|grep -E 'write:'|grep -Eoi 'IOPS=[0-9k.]+'|cut -d'=' -f2)
+    fi
+
+    echo All tests complete.
+    echo
+    echo ==================
+    echo = Dbench Summary =
+    echo ==================
+    echo "Random Read/Write IOPS: $READ_IOPS_VAL/$WRITE_IOPS_VAL. BW: $READ_BW_VAL / $WRITE_BW_VAL"
+    if [ -z $FIO_QUICK ] || [ "$FIO_QUICK" == "no" ]; then
+        echo "Average Latency (usec) Read/Write: $READ_LATENCY_VAL/$WRITE_LATENCY_VAL"
+        echo "Sequential Read/Write: $READ_SEQ_VAL / $WRITE_SEQ_VAL"
+        echo "Mixed Random Read/Write IOPS: $RW_MIX_R_IOPS/$RW_MIX_W_IOPS"
+    fi
+
+    rm $FIO_MOUNTPOINT/fiotest
+    exit 0
+fi
+
+exec "$@"
